2935b6f464a3914c53916b9e6ee2fb61dfc997a8
simon <psiplace@netscape.net>
2017-03-09 15:36:09 +0000

    comment

--- doc.go
+++ doc.go
@@ -10,40 +10,40 @@ operations are selectively parallel.
 
 arrays selectively broken into chunks for optimised parallelization.
 
-Overview/docs: [![GoDoc](https://godoc.org/github.com/splace/tensor3?status.svg)](https://godoc.org/github.com/splace/tensor3)
-
 
 Example:  100 x 1 million matrix multiplications, single threaded then parallel.
 
 ```
-package main
+	package main
 
-import . "github.com/splace/tensor3"
-import "time"
-import "fmt"
+	import . "github.com/splace/tensor3"
+	import "time"
+	import "fmt"
 
-func main(){
-	ms := make(Matrices, 1000000)
-	for i := range ms {
-		ms[i] = NewMatrix(1, 2, 3,4, 5, 6,7, 8, 9)
-	}
-	m := NewMatrix(9, 8, 7,6, 5, 4,3, 2, 1)
-	start:=time.Now()
+	func main(){
+		ms := make(Matrices, 1000000)
+		for i := range ms {
+			ms[i] = NewMatrix(1, 2, 3,4, 5, 6,7, 8, 9)
+		}
+		m := NewMatrix(9, 8, 7,6, 5, 4,3, 2, 1)
+		start:=time.Now()
 	
-	for i := 0; i < 100; i++ {
-		ms.Product(m)
+		for i := 0; i < 100; i++ {
+			ms.Product(m)
+		}
+		fmt.Println(time.Since(start))	
+		Parallel=true
+		start=time.Now()
+		for i := 0; i < 100; i++ {
+			ms.Product(m)
+		}
+		fmt.Println(time.Since(start))	
 	}
-	fmt.Println(time.Since(start))	
-	Parallel=true
-	start=time.Now()
-	for i := 0; i < 100; i++ {
-		ms.Product(m)
-	}
-	fmt.Println(time.Since(start))	
-}
 ```
 
 */
 package tensor3
+
+// Overview/docs: [![GoDoc](https://godoc.org/github.com/splace/tensor3?status.svg)](https://godoc.org/github.com/splace/tensor3)
 
 
