f6d0a65800bc62eb0b96a7783fb70f7ccde014d8
simon <psiplace@netscape.net>
2018-04-26 01:53:12 +0100

    go 1.10

--- options/main.go
+++ options/main.go
@@ -27,11 +27,6 @@ func main(){
 	fmt.Println(time.Since(start))	
 }
 
-/*  Hal3 Sun 9 Apr 18:53:12 BST 2017 go version go1.6.2 linux/amd64
-4.275177412s
-4.027586475s
-Sun 9 Apr 18:53:21 BST 2017
-*/
 /*  Hal3 Sun 9 Apr 18:53:36 BST 2017 go version go1.6.2 linux/amd64
 2.803393529s
 2.065988983s
@@ -42,5 +37,11 @@ Sun 9 Apr 18:53:42 BST 2017
 2.833770341s
 2.041434037s
 Sun 9 Apr 18:53:55 BST 2017
+*/
+/*  Hal3 Thu 26 Apr 01:49:45 BST 2018  go version go1.10 linux/amd64
+
+2.976279541s
+2.705235734s
+Thu 26 Apr 01:49:52 BST 2018
 */
 
