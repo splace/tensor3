4efd54c0de2ac4e532a671d254ea0c491f528f88
simon <psiplace@netscape.net>
2018-05-19 00:31:11 +0100

    regional chunking

--- chunking.go
+++ chunking.go
@@ -22,19 +22,23 @@ var Parallel bool
 // only improves performance if using costly functions, non of the built-ins are likely to benefit. YRMV.
 var ParallelComponents bool
 
+func chunkSize(l uint) uint {
+	if !Hints.ChunkSizeFixed {
+		if cs := l / (Hints.Threads + 1); cs > Hints.DefaultChunkSize {
+			return cs
+		}
+	}
+	return Hints.DefaultChunkSize
+}
+
 // return a channel of Vectors that are chunks of the passed Vectors
 func vectorsInChunks(vs Vectors) chan Vectors {
 	c := make(chan Vectors, 1)
-	chunkSize := Hints.DefaultChunkSize
-	if !Hints.ChunkSizeFixed {
-		if cs := uint(len(vs)) / (Hints.Threads + 1); cs > Hints.DefaultChunkSize {
-			chunkSize = cs
-		}
-	}
-	lastSplitMax := uint(len(vs))-chunkSize
+	cs:=chunkSize(uint(len(vs)))
+	lastSplitMax := uint(len(vs))-cs
 	go func() {
 		var bottom uint
-		for top := chunkSize; top < lastSplitMax; top += chunkSize {
+		for top := cs; top < lastSplitMax; top += cs {
 			c <- vs[bottom:top]
 			bottom = top
 		}
@@ -47,16 +51,11 @@ func vectorsInChunks(vs Vectors) chan Vectors {
 // return a channel of Matrices that are chunks of the passed Matrices
 func matricesInChunks(ms Matrices) chan Matrices {
 	c := make(chan Matrices)
-	chunkSize := Hints.DefaultChunkSize
-	if !Hints.ChunkSizeFixed {
-		if cs := uint(len(ms)) / (Hints.Threads + 1); cs > Hints.DefaultChunkSize {
-			chunkSize = cs
-		}
-	}
-	lastSplitMax := uint(len(ms))-chunkSize
+	cs:=chunkSize(uint(len(ms)))
+	lastSplitMax := uint(len(ms))-cs
 	go func() {
 		var bottom uint
-		for top := chunkSize; top < lastSplitMax; top += chunkSize {
+		for top := cs; top < lastSplitMax; top += cs {
 			c <- ms[bottom:top]
 			bottom = top
 		}
@@ -69,20 +68,86 @@ func matricesInChunks(ms Matrices) chan Matrices {
 // return a channel of VectorRefs that are chunks of the passed VectorRefs
 func vectorRefsInChunks(vs VectorRefs) chan VectorRefs {
 	c := make(chan VectorRefs, 1)
-	chunkSize := Hints.DefaultChunkSize
-	if !Hints.ChunkSizeFixed {
-		if cs := uint(len(vs)) / (Hints.Threads + 1); cs > Hints.DefaultChunkSize {
-			chunkSize = cs
-		}
-	}
-	lastSplitMax := uint(len(vs))-chunkSize
+	cs:=chunkSize(uint(len(vs)))
+	lastSplitMax := uint(len(vs))-cs
 	go func() {
 		var bottom uint
-		for top := chunkSize; top < lastSplitMax; top += chunkSize {
+		for top := cs; top < lastSplitMax; top += cs {
 			c <- vs[bottom:top]
 			bottom = top
 		}
 		c <- vs[bottom:]
+		close(c)
+	}()
+	return c
+}
+
+
+// return a channel of VectorRefs that are chunks of the passed VectorRefs.
+// as an optimisation, which some functions might benefit from, the VectorRefs are reordered so that each chunk contains all/only the values within a region. nearby points are MUCH more likely to be in the same chunk.
+func vectorRefsInRegionalChunks(vs VectorRefs) chan VectorRefs {
+	c := make(chan VectorRefs, 1)
+	cs:=chunkSize(uint(len(vs)))
+	if cs>uint(len(vs)){
+		c <- vs
+		close(c)
+		return c
+	}
+	go func() {
+		// sample 5% of points to make guess at distribution
+		// TODO sample cubed root number of points?
+		sp:=make(VectorRefs,len(vs)/20)
+		for i:=range(sp){
+			sp[i]=vs[i*20]
+		}
+		// TODO improve this fixed 8-way only scheme. but if less than 8 cores, is there much point?
+		// TODO regions to give chunks of a size similar to other chunking functions. 
+		average:=sp.Sum()
+		divisor:=BaseType(len(sp))
+		average.x/=divisor
+		average.y/=divisor
+		average.z/=divisor
+		//(&average).Divide(BaseType(len(sp)))
+		var chunks [2][2][2]VectorRefs
+		for _,v := range(vs){
+			if v.x>average.x {
+				if v.y>average.y {
+					if v.z>average.z {
+						chunks[1][1][1]=append(chunks[1][1][1],v)
+					}else{
+						chunks[1][1][0]=append(chunks[1][1][0],v)
+					}
+				}else{
+					if v.z>average.z {
+						chunks[1][0][1]=append(chunks[1][0][1],v)
+					}else{
+						chunks[1][0][0]=append(chunks[1][0][0],v)
+					}
+				}
+			}else{
+				if v.y>average.y {
+					if v.z>average.z {
+						chunks[0][1][1]=append(chunks[0][1][1],v)
+					}else{
+						chunks[0][1][0]=append(chunks[0][1][0],v)
+					}
+				}else{
+					if v.z>average.z {
+						chunks[0][0][1]=append(chunks[0][0][1],v)
+					}else{
+						chunks[0][0][0]=append(chunks[0][0][0],v)
+					}
+				}
+			}
+		}
+		c <- chunks[0][0][0]
+		c <- chunks[0][0][1]
+		c <- chunks[0][1][0]
+		c <- chunks[0][1][1]
+		c <- chunks[1][0][0]
+		c <- chunks[1][0][1]
+		c <- chunks[1][1][0]
+		c <- chunks[1][1][1]
 		close(c)
 	}()
 	return c
